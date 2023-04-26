861a0e93c89784077f9af834c2975062577e6f54
Jiaqi Ye <jye387@gmail.com>
2023-04-26 11:41:21 -0400

    preprocess completed

--- extract_features.py
+++ extract_features.py
@@ -2,10 +2,37 @@
 
 import librosa
 import librosa.display
-import matplotlib.pyplot as plt
+import os
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+# import matplotlib.pyplot as plt
 
 # returns a matrix of mel frequency cepstral coefficients (13 rows, one for each coefficient vs columns for frames)
 def extract_mfcc(file_name):
     signal, sample_rate = librosa.load(file_name)
     mfccs = librosa.feature.mfcc(signal, n_mfcc=13, sr=sample_rate)
     return mfccs
+
+def preprocess(dataset_path):
+    data = {"mfcc": [], "classes": []}
+    # os.walk returns a generator that creates a tuple of values (current path, directories in current path, files in current path)
+    # enumerate returns an iterator
+    for label_index, (path, dirs, files) in enumerate(os.walk(dataset_path)): # for example, label_index = 1 for all files in the "blues" folder
+        for file_name in sorted(files):
+            if file_name.endswith(".wav"):
+                dir = file_name.split(".")[0]
+                try:
+                    mfcc = extract_mfcc(os.path.join(dataset_path, dir, file_name)).T
+                    data["mfcc"].append(mfcc.tolist()) # add the frames as a list as the features
+                    data["classes"].append(label_index)
+                except:
+                    continue
+    return data
+
+# shuffles and splits data into training and test sets
+def split_data(data):
+    features = np.array(data["mfcc"])
+    classes = np.array(data["classes"])
+    shuffle(features, classes)
+    return train_test_split(features, classes, test_size=0.2, train_size=0.8, shuffle=False) # features_train, features_test, labels_train, labels_test
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import extract_features
 
 if __name__ == '__main__':
-    extract_features.extract_mfcc("music_sample_1.wav")
+    print(extract_features.preprocess("Data\genres_original"))
