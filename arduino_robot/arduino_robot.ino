b757d8e5dd6a1f23c5f9ba4bfb407c36b49a6d23
Jiaqi Ye <jye387@gmail.com>
2023-05-12 19:33:02 -0400

    added some dance moves comments

--- arduino_robot/arduino_robot.ino
+++ arduino_robot/arduino_robot.ino
@@ -6,10 +6,18 @@ Servo leftShoulder; // create servo object to control a servo
 Servo rightShoulder;
 Servo rightElbow;
 Servo leftElbow;
+Servo leftLeg;
+Servo rightLeg;
+Servo leftKnee;
+Servo rightKnee;
+Servo body;
+Servo head;
 
 int genre;
 int selection;
 double tempo;
+
+// left upper arm add to move forward
 
 void setup()
 {
@@ -22,7 +30,43 @@ void setup()
 
 //----------------------Dance Moves-----------------------
   // 1: blues
+void slowDrag(double speed) {
+  // hold arms out
+  // sway from side to side slowly
+}
+void texasShuffle(double speed) {
+  // open legs to the right
+  // arms up open to the left
+  // delay
+  // close legs at middle
+  // bring arms closer to center
+  // delay
+  // open legs to the left
+  // arms up and open to the right
+}
+void savoyWalk(double speed) {
+  // one arm up, holding hand out
+  // one arm extended and curled
+  // left leg out, right leg back
+  // delay
+  // right leg out, left leg back
+  // delay
+  // left leg out, right leg back
+  // much shorter delay
+  // right leg out, left leg back
+  // left leg out, right leg back
+  // much shorter delay
+  // right leg out, left leg back
+}
   // 2: classical
+void pirouette(double speed) {
+  // both arms up and clasped above head
+  // one leg straight down
+  // other leg at right angle and foot touching other leg
+}
+void plie(double speed) {
+  // bend both legs to make a diamond shape
+}
 void arabesque(double speed)
 {
   leftShoulder.write(180);
@@ -34,7 +78,17 @@ void arabesque(double speed)
   delay(speed);
 }
   // 3: country
+
   // 4: disco
+void ymca(double speed){
+  // both arms out at 45 degrees outstretched and two legs straight down
+  // keep legs straight, but move arms to make an "M" shape above head
+  // move one arm down (but still curled) to make "C" shape
+  // keep legs straight, but move arms to make an "M" shape above head
+}
+void discoFinger(double speed) {
+  // move one arm up and down, delay for funkyness
+}
 void robot(double speed)
 {
   rightShoulder.write(50);
@@ -59,6 +113,17 @@ void robot(double speed)
   delay(speed);
 }
   // 5: hiphop
+void wave(double speed) {
+  // start out with both arms extended straight out
+  // make left arm an L shape
+  // make left arm an upside down L shape
+  // make left arm straight and right arm upside down L shape
+  // make right arm straight
+  // make right arm L shape
+}
+void popChest(double speed) {
+  //move shoulders slightly back and forth rapidly
+}
 void runningMan(double speed)
 {
   leftShoulder.write(60);
@@ -76,6 +141,12 @@ void runningMan(double speed)
 }
   // 6: jazz
   // 7: metal
+void bobHead(double speed) {
+
+}
+void pumpLegs(double speed) {
+  // same as pumpArms but with legs
+}
 void pumpArms(double speed)
 {
   leftShoulder.write(0);
@@ -87,21 +158,33 @@ void pumpArms(double speed)
   delay(speed);
 }
   // 8: pop
+void dab(double speed) {
+  // lean to side
+  // move head to look to side at one arm
+  // move arm up and bend at elbow
+}
+void bodyRoll(double speed) {
+  // sway sensually side to side at a medium tempo
+}
+void floss(double speed) {
+  // arms are straight, left arm is back and right arm is forward, alternate between these
+}
   // 9: reggae
   // 10: rock
+      // everything's the same as for metal
 //----------------------END Dance Moves-----------------------
 
 // all the dances
-DanceFunction bluesDances[] = {}; // 1
-DanceFunction classicalDances[] = {arabesque}; // 2
+DanceFunction bluesDances[] = {slowDrag, texasShuffle, savoyWalk}; // 1
+DanceFunction classicalDances[] = {pirouette, plie, abaresque}; // 2
 DanceFunction countryDances[] = {}; // 3
-DanceFunction discoDances[] = {robot}; // 4
-DanceFunction hiphopDances[] = {runningMan}; // 5
+DanceFunction discoDances[] = {ymca, discoFinger, robot}; // 4
+DanceFunction hiphopDances[] = {wave, popChest, runningMan}; // 5
 DanceFunction jazzDances[] = {}; // 6
-DanceFunction metalDances[] = {pumpArms}; // 7
-DanceFunction popDances[] = {}; // 8
+DanceFunction metalDances[] = {bobHead, pumpLegs, pumpArms}; // 7
+DanceFunction popDances[] = {dab, bodyRoll, floss}; // 8
 DanceFunction reggaeDances[] = {}; // 9
-DanceFunction rockDances[] = {pumpArms}; // 10
+DanceFunction rockDances[] = {bobHead, pumpLegs, pumpArms}; // 10
 
 void loop()
 {
